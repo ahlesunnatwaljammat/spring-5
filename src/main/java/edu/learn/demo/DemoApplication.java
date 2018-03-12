1176e1d81126abe41b1670d65e482ef6b687b962
noman.ali.abbasy@gmail.com <Engrabbasi1>
2018-03-12 15:46:41 +0500

    Added wiki controller

--- src/main/java/edu/learn/demo/DemoApplication.java
+++ src/main/java/edu/learn/demo/DemoApplication.java
@@ -2,20 +2,14 @@ package edu.learn.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
+@ComponentScan(basePackages = {"edu.learn.rest"})
 public class DemoApplication {
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
